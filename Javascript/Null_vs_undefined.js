f0c1f76cd63ed7416b36ca606ada6d1a7ace4287
Sara Manzoor <saramanzoor76@gmail.com>
2026-08-12 14:39:29 +0500

    Practice Null vs Undefined in js

---
+++ Javascript/Null_vs_undefined.js
@@ -0,0 +1,24 @@
+//undefined
+//Variable declare hui hai but value assign nahi hui.
+
+let age;
+
+console.log(age);
+//undefined
+
+
+//Null 
+//Intentionally empty value represent karne ke liye use hota hai.
+
+let address=null;
+
+console.log(address);
+//null
+
+//Main Points
+//undefined usually means a value has not been assigned, while null represents an intentional absence of value.
+
+
+//type of undefined is undefined and type of null is object
+
+console.log(typeof age);
