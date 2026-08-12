d6d94b285732dfb92d56bc430382aa5215c780e1
Sara Manzoor <saramanzoor76@gmail.com>
2026-08-12 14:41:18 +0500

    Practice Template Literals

---
+++ Javascript/Template_Literals.js
@@ -0,0 +1,11 @@
+//Template Literals
+//use backticks instead of single or double quotes
+let name="Sara";
+let age=21;
+
+let message=`Hello, my name is ${name} and I am ${age} years old.`;
+console.log(message);
+//Hello, my name is Sara and I am 21 years old.
+
+console.log(`2 + 3 = ${2 + 3}`);
+//5
