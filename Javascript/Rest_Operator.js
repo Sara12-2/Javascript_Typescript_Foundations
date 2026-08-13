bc865779bc36ee602e25be11eb0285b1cdf4305a
Sara Manzoor <saramanzoor76@gmail.com>
2026-08-13 13:53:21 +0500

    Practicing Rest operator in js

---
+++ Javascript/Rest_Operator.js
@@ -0,0 +1,12 @@
+//Rest operator 
+//Rest opertaor Multiple values ko ek single array me convert karta hai
+
+function sum(...numbers) {
+    console.log(numbers);
+}
+
+sum(10, 20, 30);
+
+
+//Main point 
+//Spread vs Rest Spread=expand and Rest=collect Same ... Syntax hota h er context differnt hota h 
