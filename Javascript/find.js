f520a88feb8a1accc6ae5065d7922639edf95e76
Sara Manzoor <saramanzoor76@gmail.com>
2026-08-13 14:21:37 +0500

    Practicing find() in js

---
+++ Javascript/find.js
@@ -0,0 +1,10 @@
+//find()
+//first matching element in the array is returned
+
+
+const numbers = [5, 10, 15, 20];
+
+const result = numbers.find(num => num > 10);
+
+console.log(result);
+//output: 15
