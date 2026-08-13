36028bf25bc355344bbd4d4233952cb79563ad1e
Sara Manzoor <saramanzoor76@gmail.com>
2026-08-13 14:20:33 +0500

    Practicing for each()in js

---
+++ Javascript/forEach.js
@@ -0,0 +1,11 @@
+//forEach()
+//array ky hr element pr function execute krta hai, or koi naya array return nahi krta
+
+
+const numbers = [1, 2, 3];
+
+numbers.forEach(num => {
+    console.log(num);
+}); 
+//output 1,2,3 
+//it does not give new transformed array, it just executes the function for each element in the array.
