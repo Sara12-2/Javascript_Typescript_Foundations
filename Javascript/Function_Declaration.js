829e3329c21f4d520120557c19d5e6803b0639cb
Sara Manzoor <saramanzoor76@gmail.com>
2026-08-15 13:19:16 +0500

    Function Declaration in js

---
+++ Javascript/Function_Declaration.js
@@ -0,0 +1,15 @@
+//function declaration
+function greet() {
+    console.log("Hello");
+}
+
+greet();
+
+//function decalaration hoisted hoti h
+
+
+greet();
+
+function greet() {
+    console.log("Hello");
+} //also works
