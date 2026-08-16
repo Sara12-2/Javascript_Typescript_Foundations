0cb132b591a322412ab6609e2ad81a01e62f633f
Sara Manzoor <saramanzoor76@gmail.com>
2026-08-16 14:27:56 +0500

    Practicing Hoisting in js

---
+++ Javascript/Hoisting.js
@@ -0,0 +1,14 @@
+//hositing
+
+//js execution s phly hmesha declaration ko process h 
+
+console.log(x);
+var x=10
+
+//undefined 
+
+
+//but 
+console.log(x);
+let x=10
+//error beaucse let temporal dead zone main h (TDZ)
