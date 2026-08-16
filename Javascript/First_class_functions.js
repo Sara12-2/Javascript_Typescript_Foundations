39a8018081b83cd5a87359d83b67f815a6849d57
Sara Manzoor <saramanzoor76@gmail.com>
2026-08-16 14:20:05 +0500

    Practicing First Class Functions in js

---
+++ Javascript/First_class_functions.js
@@ -0,0 +1,13 @@
+//first class functions 
+//js main function value ki trh treat hoty hain 
+
+const greet = function () {
+    return "Hello";
+};
+
+const fn = greet;
+
+console.log(fn());
+
+//main points 
+//function ko variabl main store kr skhty haun argument pass kr skhty hain and then return kr skhty hain 
